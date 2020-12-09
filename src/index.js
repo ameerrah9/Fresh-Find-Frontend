f11fdab4ffb705bdaa8e1f123ef0707caa2b546d
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-12-08 19:37:56 -0500

    Connecting JS to HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("Hi")
+const BASEURL = 'http://localhost:3000/lists'
+//console.log(BASEURL) ^(control) + option + L
+console.log(BASEURL)
+
+//sti
+setInterval(() => {
+    
+}, 1000)
+
+//sto
+setTimeout(() => {
+    
+}, 1000)
+
+//clg
+//console.log(object)
+
+//OO in JS
+// class Person {
+//     constructor(name, age) {
+//       this.name = name;
+//       this.age = age;
+//     }
+class List {
+    constructor(name) {
+      this.name = name;
+    }
+
+class Item {
+  constructor(content) {
+    this.content = content;
+  }
+
+//let evan = new Person('Evan', 34);
+  

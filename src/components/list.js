bb20807cd29872bede34a59e96c8ad5a2117b217
Mike Rogers <me@mikerogers.io>
2020-12-30 13:16:04 +0000

    Removing <br>'s using tailwind classes

--- src/components/list.js
+++ src/components/list.js
@@ -28,18 +28,21 @@ class List {
     createListCard() {
         const li = document.createElement('li')
         li.dataset.id = this.id
-        li.className = "my-2 px-4 bg-green-200"
+        li.className = "my-0 px-4 bg-green-200"
     
         // const h1 = document.createElement('h1')
         // h1.className = "text-2xl semibold border-b-4 border-blue"
         // h1.innerHTML = "My First Item!"
 
-        const p = document.createElement('p')
-        p.className = "text-2xl semibold border-b-2"
-        p.innerHTML = `<br>${this.name}<br><br>`
+        const h1 = document.createElement('h1')
+        h1.className = "text-3xl semibold border-b-2 py-3"
+        h1.innerHTML = `${this.name}`
     
         const deleteBtn = document.createElement("button")
-        deleteBtn.innerHTML = `<br><div class="text-2xl semibold border-b-4 border-red">Delete List <a href="#" class="my-4 text-right"><i class="fa fa-trash-alt"></i></a></div>`
+        deleteBtn.className = "text-xl semibold float-right p-3 mt-1 ml-4"
+        deleteBtn.innerHTML = `
+          <i class="fa fa-trash-alt"></i>
+        `
         deleteBtn.addEventListener("click", this.deleteList)
     
         const itemList = document.createElement('ul')
@@ -53,7 +56,7 @@ class List {
         itemForm.innerHTML += 
         `
         <br>
-        <div class="text-2xl">Add an Item Here:</div>
+        <div class="text-xl">Add an Item Here:</div>
         <input type="text" name="input" id="item-input" class="flex-1 p-3" placeholder="New Item"/>
         <button type="submit" class="flex-none"><i class="fa fa-plus p-4 z--1 bg-green-400"></i></button>
         </div>
@@ -62,7 +65,7 @@ class List {
         `
         itemForm.addEventListener("submit", Item.createItem)
 
-        li.append(p, itemList, deleteBtn, itemForm)
+        li.append(deleteBtn, h1, itemList, itemForm)
     
         listUL.appendChild(li)
         
